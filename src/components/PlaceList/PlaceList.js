5790a2a37b0b01362fe9e563d92856179456b628
Vivek Mishra <vivek.mishra@cdk.com>
2019-01-18 09:00:01 +0530

    Broke app component into smaller components.

---
+++ src/components/PlaceList/PlaceList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {View, StyleSheet} from 'react-native';
+import ListItem from '../ListItem/ListItem';
+
+const placeList = props => {
+    const placesOutput = props.places.map((place, i) => (
+        <ListItem key={i} placeName={place}></ListItem>
+    ));
+    return (
+        <View style={styles.listContainer}>{placesOutput}</View>
+    );
+}
+
+const styles = StyleSheet.create({
+    listContainer: {
+        width: "100%"
+    }
+});
+
+export default placeList;

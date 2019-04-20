80ea8cc9d53d31f9e1a003ab2cb0d03319586739
Vivek Mishra <vivek.mishra@cdk.com>
2019-04-20 11:02:37 +0530

    Added react native navigation support

--- App.js
+++ App.js
@@ -1,73 +1,17 @@
-import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
+import { Navigation } from 'react-native-navigation';
+import AuthScreen from './src/screens/Auth/Auth';
+import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
+import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 
-import { connect } from 'react-redux';
+// Register screens.
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen);
 
-import PlaceInput from "./src/components/PlaceInput/PlaceInput";
-import PlaceList from "./src/components/PlaceList/PlaceList";
-import placeImage from "./src/assets/seychelles.jpeg";
-import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
-import { addPlace, deletePlace, selectPlace, deSelectPlace } from './src/store/actions';
-
-class App extends Component {
-  placeAddedHandler = placeName => {
-    this.props.onAddPlace(placeName);
-  };
-
-  placeDeletedHandler = () => {
-    this.props.onDeletePlace();
-  }
-
-  modalClosedHandler = () => {
-    this.props.onDeSelectPlace();
-  }
-
-  placeSeletedHandler = key => {
-    this.props.onSelectPlace(key);
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetail 
-          selectedPlace={this.props.selectedPlace} 
-          onItemDeleted={this.placeDeletedHandler} 
-          onModalClosed={this.modalClosedHandler}
-        />
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList
-          places={this.props.places}
-          onItemSeleted={this.placeSeletedHandler}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 26,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-start"
+// Start App (Single screen without tab)
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "awesome-places.AuthScreen",
+    title: "Login"
   }
 });
-
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  };
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: (name) => dispatch(addPlace(name)),
-    onDeletePlace: () => dispatch(deletePlace()),
-    onSelectPlace: (key) => dispatch(selectPlace(key)),
-    onDeSelectPlace: () => dispatch(deSelectPlace())
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
--- android/app/src/main/java/com/rnapp/MainApplication.java
+++ android/app/src/main/java/com/rnapp/MainApplication.java
@@ -12,36 +12,69 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.ReactGateway;
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
+public class MainApplication extends NavigationApplication {
+  // @Override
+  // protected ReactGateway createReactGateway() {
+  //   ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+  //     @Override
+  //     protected String getJSMainModuleName() {
+  //       return "index";
+  //     }
+  //   };
+  //   return new ReactGateway(this, isDebug(), host);
+  // }
+  @Override
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+  protected List<ReactPackage> getPackages() {
+      // Add additional packages you require here
+      // No need to add RnnPackage and MainReactPackage
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-          , new VectorIconsPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
+        new VectorIconsPackage()
+    );
+  }  
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
   }
 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public String getJSMainModuleName() {
+    return "index";
   }
+
+  // private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+  //   @Override
+  //   public boolean getUseDeveloperSupport() {
+  //     return BuildConfig.DEBUG;
+  //   }
+
+  //   @Override
+  //   protected List<ReactPackage> getPackages() {
+  //     return Arrays.<ReactPackage>asList(
+  //         new MainReactPackage()
+  //         , new VectorIconsPackage()
+  //     );
+  //   }
+
+  //   @Override
+  //   protected String getJSMainModuleName() {
+  //     return "index";
+  //   }
+  // };
+
+  // @Override
+  // public ReactNativeHost getReactNativeHost() {
+  //   return mReactNativeHost;
+  // }
+
+  // @Override
+  // public void onCreate() {
+  //   super.onCreate();
+  //   SoLoader.init(this, /* native exopackage */ false);
+  // }
 }
